0ee8bf295cb438fc4de5da3ed71c15cc2ef285bf
ashkank <ashkank@umich.edu>
2019-08-29 17:46:43 -0400

    WIP writing the paragraph ranking code

---
+++ paragraph_ranking/rank.py
@@ -0,0 +1,14 @@
+
+
+# gets as input a news article, a question and
+# the explanation for the questions answer.
+# breaks the article in to paragraphs and
+# creates a graph representation of them with
+# textrank_utilities.build_textrank_graph
+# runs the textrank algorithm on it and returns
+# the ranking of each paragraph as a score.
+def rank_paragraphs(text, q, exp):
+    return
+
+
+def run_pagerank(matrix):
---
+++ paragraph_ranking/textrank_utilities.py
@@ -0,0 +1,35 @@
+
+
+# cosine similarity calculator between two vectors,
+# gets two vectors as input and return their
+# cosine similarity, a value between [-1, 1]
+def cosine_similarity(a, b):
+    return
+
+
+# gets as input two paragraph of text
+# breaks them down into tokens, calculates
+# the word2vec word embeddings for all tokens
+# and then calculates the average of these
+# embeddings for each input. Then calculates
+# cosine similarity between the two vectors
+# and returns a number in range [-1, 1]
+def calculate_similarity(a, b):
+    # calculate average word embeddings
+    # for a and b
+    # return cosine_similarity(avg_embeddings_a, avg_embeddings_b)
+    return
+
+
+# gets as input a body of text, including
+# \n separated paragraphs. Outputs those
+# paragraphs in a list of strings.
+def get_paragraphs(text):
+    return
+
+
+# gets an input a list of paragraphs.
+# returns a directed graphs, as described
+# in the paper, with all the weights assigned
+def build_textrank_graph(paragraphs):
+    return
