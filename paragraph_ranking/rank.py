36667cc693345c2e21d6d7d915f6bf968d52736c
ashkank <ashkank@umich.edu>
2019-08-29 21:02:31 -0400

    WIP writing the paragraph ranking code

--- paragraph_ranking/rank.py
+++ paragraph_ranking/rank.py
@@ -6,9 +6,16 @@
 # creates a graph representation of them with
 # textrank_utilities.build_textrank_graph
 # runs the textrank algorithm on it and returns
-# the ranking of each paragraph as a score.
+# the ranking of each paragraph as a score,
+# alongside the paragraph itself.
 def rank_paragraphs(text, q, exp):
     return
 
 
+# gets as input a matrix of size n x n
+# runs page_rank algorithm on it with
+# s = 0.85 and returns the ranking in
+# a vector of nodes, alongside with
+# their rank value.
 def run_pagerank(matrix):
+    return
