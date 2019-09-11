605f3969818ed56cd51fb64bcf99500e5dea9104
ashkank <ashkank@umich.edu>
2019-09-10 21:03:38 -0400

    WIP debug

--- paragraph_ranking/rank.py
+++ paragraph_ranking/rank.py
@@ -100,6 +100,7 @@ def rank_train_data_for_question(qid):
         sorted_rankings = [x for _, x in sorted(zip(exp_similarities, paragraphs), key=lambda pair: pair[0])]
 
         new_text = ''
+        print('pooo' + str(len(sorted_rankings)))
         for i in range(NUMBER_OF_TOP_PARAGRAPHS_TO_INCLUDE):
             new_text += sorted_rankings[-i] + '\n'
 
@@ -120,7 +121,6 @@ def rank_train_data_for_question(qid):
 
         sorted_rankings = [x for _, x in sorted(paragraph_rankings, key=lambda pair: pair[0])]
         new_text = ''
-        print('pooo' + str(len(sorted_rankings)))
         for i in range(NUMBER_OF_TOP_PARAGRAPHS_TO_INCLUDE):
             new_text += sorted_rankings[-i] + '\n'
 
