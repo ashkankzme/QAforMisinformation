03f9d7526545c3f707d438d040751ef989d71c46
Ashkan Kazemi <ashkank@lit1000.eecs.umich.edu>
2019-10-12 08:28:09 -0400

    updating stuff on server

--- qa/qa_xlnet_whole_doc.py
+++ qa/qa_xlnet_whole_doc.py
@@ -105,7 +105,7 @@ train_masks = torch.tensor(train_masks)
 validation_masks = torch.tensor(validation_masks)
 
 # Select a batch size for training. For fine-tuning with XLNet, the authors recommend a batch size of 32, 48, or 128. We will use 32 here to avoid memory issues.
-batch_size = 1
+batch_size = 4
 
 # Create an iterator of our data with torch DataLoader. This helps save on memory during training because, unlike a for loop,
 # with an iterator the entire dataset does not need to be loaded into memory
@@ -148,7 +148,7 @@ def flat_accuracy(preds, labels):
 
 
 # Number of training epochs (authors recommend between 2 and 4)
-epochs = 15
+epochs = 20
 
 # trange is a tqdm wrapper around the normal python range
 for _ in trange(epochs, desc="Epoch"):
