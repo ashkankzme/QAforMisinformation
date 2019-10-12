db613cb097c4284bba9ecc3e900e2f30298c63f2
ashkank <ashkank@umich.edu>
2019-10-12 10:53:24 -0400

    changed max len.

--- qa/qa_xlnet_whole_doc.py
+++ qa/qa_xlnet_whole_doc.py
@@ -44,7 +44,7 @@ print ("Tokenize the first sentence:")
 print (tokenized_texts_train[0])
 
 # Set the maximum sequence length. The longest sequence in our training set is 47, but we'll leave room on the end anyway.
-MAX_LEN = 320
+MAX_LEN = 340
 average_len = 0
 reduced_inputs = 0
 for tokens in tokenized_texts_train + tokenized_texts_dev + tokenized_texts_test:
