6dc37f4c7b79e253b4144e68723af4befc30bd41
ashkank <ashkank@umich.edu>
2019-10-12 11:59:34 -0400

    more training.

--- qa/qa_xlnet_whole_doc.py
+++ qa/qa_xlnet_whole_doc.py
@@ -149,7 +149,7 @@ def flat_accuracy(preds, labels):
 
 
 # Number of training epochs (authors recommend between 2 and 4)
-epochs = 25
+epochs = 35
 
 # trange is a tqdm wrapper around the normal python range
 for _ in trange(epochs, desc="Epoch"):
