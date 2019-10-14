0202f68bac2cbfd030ac133a61f64db39912a8cf
ashkank <ashkank@umich.edu>
2019-10-14 10:28:29 -0400

    less training to see if we are overfitting or not.

--- qa/qa_xlnet_whole_doc_no_validation.py
+++ qa/qa_xlnet_whole_doc_no_validation.py
@@ -133,7 +133,7 @@ def flat_accuracy(preds, labels):
 
 
 # Number of training epochs (authors recommend between 2 and 4)
-epochs = 30
+epochs = 10
 
 # trange is a tqdm wrapper around the normal python range
 for _ in trange(epochs, desc="Epoch"):
