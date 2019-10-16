1d21a7e7e5184c9309ce8e3e87ba370997f496db
ashkank <ashkank@umich.edu>
2019-10-16 03:33:58 -0400

    changed ranking language model to BERT

--- paragraph_ranking/utils.py
+++ paragraph_ranking/utils.py
@@ -6,10 +6,10 @@ import nltk
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-# model = BertModel.from_pretrained('bert-base-uncased')
-tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
-model = XLNetModel.from_pretrained('xlnet-base-cased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+# tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
+# model = XLNetModel.from_pretrained('xlnet-base-cased')
 if torch.cuda.device_count() > 1:
   print("Let's use", torch.cuda.device_count(), "GPUs!")
   # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
