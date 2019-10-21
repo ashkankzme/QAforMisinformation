39e97e862e8474261ca475b75f4b3094db7e4429
ashkan <ashkank@umich.edu>
2019-10-21 12:44:46 -0400

    smaller document length :(

--- qa/qa_xlnet_long_doc_no_validation.py
+++ qa/qa_xlnet_long_doc_no_validation.py
@@ -45,7 +45,7 @@ print("Tokenize the first sentence:")
 print(tokenized_texts_train[0])
 
 # Set the maximum sequence length. The longest sequence in our training set is 47, but we'll leave room on the end anyway.
-MAX_LEN = 2400
+MAX_LEN = 2300
 average_len = 0
 to_be_deleted = []
 for i, tokens in enumerate(tokenized_texts_train + tokenized_texts_test):
