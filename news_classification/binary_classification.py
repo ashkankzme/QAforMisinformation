0bd73913ff1a17ac3298b43b1fcdc4388e1a63d6
ashkank <ashkank@umich.edu>
2019-10-23 12:53:29 -0400

    more training for the multi-class classification task.

--- news_classification/binary_classification.py
+++ news_classification/binary_classification.py
@@ -180,7 +180,7 @@ def flat_accuracy(preds, labels):
 
 
 # Number of training epochs (authors recommend between 2 and 4)
-epochs = 15
+epochs = 20
 
 # trange is a tqdm wrapper around the normal python range
 for epoch in trange(epochs, desc="Epoch"):
