b4916a7024e6ca58bcb48497531b8b4422c8ade5
ashkan <ashkank@umich.edu>
2019-12-07 15:29:45 -0500

    hitrate calculation code ready

--- paragraph_ranking/evaluate_ranking.py
+++ paragraph_ranking/evaluate_ranking.py
@@ -1,5 +1,8 @@
 import re, json, string
+from utils import get_bert_embeddings, cosine_similarity
+
 translator = str.maketrans('', '', string.punctuation)
+
 
 # returns all the text between qoutations
 # in the input text as a list.
@@ -34,19 +37,55 @@ def get_ranking_recall(articles):
     return found, len(selected_articles)
 
 
+def calculate_hitrate(articles):
+    hit = 0
+    for article in articles:
+        if article['answer'] in [0, 2] and 1 not in article['paragraph_relevance_learned_labels']:
+            hit += 1
+            continue
+
+        paragraphs = article['paragraphs']
+        paragraphs_embeddings = [get_bert_embeddings(p) for p in paragraphs]
+        exp_embeddings = get_bert_embeddings(article['explanation'])
+        similarities = [0] * len(paragraphs_embeddings)
+        similarity_map = {}
+        for i, pe in enumerate(paragraphs_embeddings):
+            similarities[i] = cosine_similarity(pe, exp_embeddings)
+            similarity_map[similarities[i]] = i
+
+        top3_similarities = sorted(similarities, key=lambda pair: pair[0], reverse=True)[:3]
+        top3_paragraph_indices = [similarity_map[sim] for sim in top3_similarities]
+        for i in top3_paragraph_indices:
+            if article['paragraph_relevance_learned_labels'][i]:
+                hit += 1
+                break
+
+    return hit/len(articles)
+
+
 def main():
     for qid in range(1, 11):
-        with open('../data/question_answering_gold_standard_fine_grained_textrank_50/q{}_train.json'.format(qid)) as train_file:
-            articles = json.load(train_file)
+        # with open('../data/question_answering_gold_standard_fine_grained_textrank_50/q{}_train.json'.format(
+        #         qid)) as train_file:
+        #     articles = json.load(train_file)
+        #
+        # with open('../data/question_answering_gold_standard_fine_grained_textrank_50/q{}_test.json'.format(
+        #         qid)) as test_file:
+        #     articles += json.load(test_file)
+        #
+        # with open('../data/question_answering_gold_standard_fine_grained_textrank_50/q{}_dev.json'.format(
+        #         qid)) as dev_file:
+        #     articles += json.load(dev_file)
+        #
+        # found, total = get_ranking_recall(articles)
+        # print('found: {}, total: {}, percentage: {}'.format(found, total, found / total))
 
-        with open('../data/question_answering_gold_standard_fine_grained_textrank_50/q{}_test.json'.format(qid)) as test_file:
-            articles += json.load(test_file)
+        with open('../data/paragraph_relevance_classification_input/q{}_test.json'.format(
+                qid)) as test_file:
+            articles = json.load(test_file)
 
-        with open('../data/question_answering_gold_standard_fine_grained_textrank_50/q{}_dev.json'.format(qid)) as dev_file:
-            articles += json.load(dev_file)
+        print('#Q{}: Hit Rate is == {}'.format(qid, calculate_hitrate(articles)))
 
-        found, total = get_ranking_recall(articles)
-        print('found: {}, total: {}, percentage: {}'.format(found, total, found/total))
 
 if __name__ == "__main__":
     main()
