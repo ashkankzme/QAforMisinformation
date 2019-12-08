26b50caf85a5d9d494a852cd98321a767f571238
ashkank <ashkank@umich.edu>
2019-12-08 10:28:24 -0500

    extraction hit rate evaluation code added.

--- paragraph_ranking/evaluate_ranking.py
+++ paragraph_ranking/evaluate_ranking.py
@@ -1,4 +1,7 @@
-import re, json, string
+import json
+import re
+import string
+
 from utils import get_bert_embeddings, cosine_similarity
 
 translator = str.maketrans('', '', string.punctuation)
@@ -60,7 +63,51 @@ def calculate_hitrate(articles):
                 hit += 1
                 break
 
-    return hit/len(articles)
+    return hit / len(articles)
+
+
+def calculate_extraction_hitrate(articles):
+    hit_1 = 0
+    hit_2 = 0
+    hit_3 = 0
+    total = 0
+    for article in articles:
+        if 'paragraph_relevance_extracted_labels' not in article:
+            continue
+
+        total += 1
+
+        paragraphs = article['paragraphs']
+        paragraphs_embeddings = [get_bert_embeddings(p) for p in paragraphs]
+        exp_embeddings = get_bert_embeddings(article['explanation'])
+        similarities = [0] * len(paragraphs_embeddings)
+        similarity_map = {}
+        for i, pe in enumerate(paragraphs_embeddings):
+            similarities[i] = cosine_similarity(pe, exp_embeddings)
+            similarity_map[similarities[i]] = i
+
+        sorted_similarities = sorted(similarities, reverse=True)
+        top3_similarities = sorted_similarities[:3]
+        top3_paragraph_indices = [similarity_map[sim] for sim in top3_similarities]
+        top2_similarities = sorted_similarities[:2]
+        top2_paragraph_indices = [similarity_map[sim] for sim in top2_similarities]
+        top_similarity = sorted_similarities[0]
+        top_paragraph_index = similarity_map[top_similarity]
+
+        if article['paragraph_relevance_extracted_labels'][top_paragraph_index]:
+            hit_1 += 1
+
+        for i in top2_paragraph_indices:
+            if article['paragraph_relevance_extracted_labels'][i]:
+                hit_2 += 1
+                break
+
+        for i in top3_paragraph_indices:
+            if article['paragraph_relevance_extracted_labels'][i]:
+                hit_3 += 1
+                break
+
+    return hit_1 / len(articles), hit_2 / len(articles), hit_3 / len(articles)
 
 
 def main():
@@ -80,11 +127,18 @@ def main():
         # found, total = get_ranking_recall(articles)
         # print('found: {}, total: {}, percentage: {}'.format(found, total, found / total))
 
-        with open('../data/paragraph_relevance_classification_input/q{}_test.json'.format(
-                qid)) as test_file:
-            articles = json.load(test_file)
+        # with open('../data/paragraph_relevance_classification_input/q{}_test.json'.format(qid)) as test_file:
+        #     articles = json.load(test_file)
+        #
+        # print('#Q{}: Hit Rate is == {}'.format(qid, calculate_hitrate(articles)))
 
-        print('#Q{}: Hit Rate is == {}'.format(qid, calculate_hitrate(articles)))
+        with open('../data/paragraph_relevance_classification_input/q{}_train.json'.format(qid)) as train_file:
+            articles = json.load(train_file)
+
+        top_hitrate, top2_hitrate, top3_hitrate = calculate_extraction_hitrate(articles)
+        print('#Q{}: Top Hit Rate is == {}, Top2 Hit Rate is == {}, Top3 Hit Rate is == {}'.format(qid, top_hitrate,
+                                                                                                   top2_hitrate,
+                                                                                                   top3_hitrate))
 
 
 if __name__ == "__main__":
