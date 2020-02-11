ed4ba66c0949462cab0a14b9417e6c32c05fc93f
ashkank <ashkank@umich.edu>
2020-02-10 20:25:56 -0500

    updating tests for biased textrank

--- paragraph_ranking/tests/test.py
+++ paragraph_ranking/tests/test.py
@@ -1,10 +1,13 @@
 from test_article2 import text, q, exp
 from rank_gold_standard import biased_textrank
+from utils import get_sentences
 
-ranks, paragraphs = biased_textrank(text, q, exp)
-sorted_rankings = sorted(zip(ranks, paragraphs), key=lambda pair: pair[0])
-top_paragraphs = [p for _, p in sorted_rankings[len(sorted_rankings)-12: ]] # top 10 pieces
 
-for rank, p in zip(ranks, paragraphs):
+texts = get_sentences(text)
+ranks, texts = biased_textrank(texts, exp)
+sorted_rankings = sorted(zip(ranks, texts), key=lambda pair: pair[0])
+top_paragraphs = [p for _, p in sorted_rankings[-10: ]] # top 10 pieces
+
+for rank, p in zip(ranks, texts):
     if p in top_paragraphs:
         print('{}'.format(p))
