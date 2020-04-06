11e51e96f687283e5be8a6f09cbe90a71880a5f0
ashkank <ashkank@umich.edu>
2020-04-06 17:35:49 -0400

    adding bias normalization to biased textrank

--- paragraph_ranking/rank_gold_standard.py
+++ paragraph_ranking/rank_gold_standard.py
@@ -89,7 +89,7 @@ def biased_textrank(texts, bias_text, damping_factor=0.8, similarity_threshold=0
     for i, text_embedding in enumerate(texts_embeddings):
         bias_text_similarities[i] = cosine_similarity(text_embedding, bias_embedding)
 
-    # bias_text_similarities = rescale(bias_text_similarities)
+    bias_text_similarities = rescale(bias_text_similarities)
 
     bias = torch.tensor(bias_text_similarities)
     scaled_matrix = damping_factor * matrix + (1 - damping_factor) * bias
